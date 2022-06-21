d27d5598326e5c8bd74cd011987c49cc0bf8f959
Loyda-Varela <73678119+Loyda-Varela@users.noreply.github.com>
2022-06-20 20:16:47 -0600

    Initial commit

---
+++ app/src/main/java/com/aplication/tarea4/Activityvolley.java
@@ -0,0 +1,14 @@
+package com.aplication.tarea4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activityvolley extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activityvolley);
+    }
+}
